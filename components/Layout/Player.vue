7e07185c112c98dba29402a5c288feae8cb5d29f
Ilya Malyukov <ilya.maliukov@yandex.ru>
2022-07-29 16:18:17 +0300

    soundmaster

--- components/Layout/Player.vue
+++ components/Layout/Player.vue
@@ -1,7 +1,19 @@
 <template lang="pug">
   .player
     .title Player
+    button(@click.prevent='soundManager.play("base", "https://enazadevkz.cdnvideo.ru/tank1/sony/A10301A0004574902L_20210331042345765/resources/ad5a61f35b99.mp3")') 
 </template>
+
+<script>
+import soundManager from 'soundmanager2'
+
+export default {
+  name: 'Player',
+  fetch: () => ({
+    soundManager: soundManager
+  }),
+}
+</script>
 
 <style lang="scss" scoped>
 @import '~styles/mixins.scss';
