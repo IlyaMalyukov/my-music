01b12888bb96fda3f8434007900a354aa5e5c2f0
Ilya Malyukov <ilya.maliukov@yandex.ru>
2022-07-29 11:51:58 +0300

    Отступы в плеере

--- components/Layout/Player.vue
+++ components/Layout/Player.vue
@@ -4,11 +4,21 @@
 </template>
 
 <style lang="scss" scoped>
+@import '~styles/mixins.scss';
+
 .player {
   background-color: #F5F5F5;
   width: 100%;
-  padding: 15px 30px;
+  padding: 15px 100px;
   position: fixed;
   bottom: 0;
+
+  @include phones {
+    padding: 15px 50px;
+  }
+
+  @include phones-small {
+    padding: 15px 15px;
+  }
 }
 </style>
