837c58a599a162f529f98c02531d594b8f90e5fd
Ilya Malyukov <ilya.maliukov@yandex.ru>
2022-07-29 10:53:17 +0300

    Доделал хэдер

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
   .header
+    img.logo(:src='require("~/assets/images/logo.svg")')
     .nav
       NuxtLink.nav__link(to='/') Каталог
+    .icon.icon-search
+    .icon.icon-menu
 </template>
 
 <style lang="scss" scoped>
@@ -11,6 +14,9 @@
   background-color: #007AFF;
   width: 100%;
   padding: 15px 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   @include phones {
     padding: 15px 50px;
@@ -22,8 +28,31 @@
 }
 
 .nav {
+  @include phones {
+    display: none;
+  }
+
   &__link {
     color: #FFF;
+  }
+}
+
+.icon {
+  color: #FFF;
+
+  &-search {
+    @include phones {
+      display: none;
+    }
+  }
+
+  &-menu {
+    display: none;
+    font-size: 25px;
+
+    @include phones {
+      display: block;
+    }
   }
 }
 </style>
