b0ae0d1ddc0c5e783f01645680274bab79e14cef
Ilya Malyukov <ilya.maliukov@yandex.ru>
2022-07-29 16:57:18 +0300

    Тест soundmaster2

--- components/Layout/Player.vue
+++ components/Layout/Player.vue
@@ -1,17 +1,19 @@
 <template lang="pug">
   .player
     .title Player
-    button(@click.prevent='soundManager.play("base", "https://enazadevkz.cdnvideo.ru/tank1/sony/A10301A0004574902L_20210331042345765/resources/ad5a61f35b99.mp3")') 
+    button(@click.prevent='soundManager.play("base", "https://enazadevkz.cdnvideo.ru/tank1/sony/A10301A0004574902L_20210331042345765/resources/ad5a61f35b99.mp3")') Play
 </template>
 
 <script>
-import soundManager from 'soundmanager2'
+if (process.client) {
+  const soundManager = require('../../plugins/soundmanager2')
+}
 
 export default {
   name: 'Player',
-  fetch: () => ({
+  data: () => ({
     soundManager: soundManager
-  }),
+  })
 }
 </script>
 
