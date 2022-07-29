c4b469ddb7125c1578229350c59b1aea7c7b17ab
Ilya Malyukov <ilya.maliukov@yandex.ru>
2022-07-29 17:28:57 +0300

    Включение/выключение трека

--- components/Layout/Player.vue
+++ components/Layout/Player.vue
@@ -1,19 +1,47 @@
 <template lang="pug">
   .player
-    .title Player
-    button(@click.prevent='soundManager.play("base", "https://enazadevkz.cdnvideo.ru/tank1/sony/A10301A0004574902L_20210331042345765/resources/ad5a61f35b99.mp3")') Play
+    button.player__button(
+      v-if='!isPlay'
+      @click.prevent='play')
+      .icon-play3.icon
+    button.player__button(
+      v-else
+      @click.prevent='pause')
+      .icon-pause2.icon
 </template>
 
 <script>
 if (process.client) {
-  const soundManager = require('../../plugins/soundmanager2')
+  const soundManager = require('soundmanager2')
 }
 
 export default {
   name: 'Player',
   data: () => ({
-    soundManager: soundManager
-  })
+    soundManager: soundManager,
+    firstTrack: true,
+    isPlay: false
+  }),
+  methods: {
+    play() {
+      this.soundManager.play('base', this.currentTrackUrl)
+      this.isPlay = true
+    },
+    pause() {
+      this.soundManager.pause('base')
+      this.isPlay = false
+    }
+  },
+  computed: {
+    currentTrackUrl() {
+      if (this.firstTrack) {
+        return 'https://enazadevkz.cdnvideo.ru/tank1/sony/A10301A0004574902L_20210331042345765/resources/ad5a61f35b99.mp3'
+      }
+
+      return 'https://enazadevkz.cdnvideo.ru/tank3/medialand/2021_05_12/1.mp3'
+    }
+  }
+
 }
 </script>
 
@@ -34,5 +62,22 @@ export default {
   @include phones-small {
     padding: 15px 15px;
   }
+
+  &__button {
+    cursor: pointer;
+    border: 2px solid #4a4a4a;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+.icon {
+  font-size: 25px;
+  color: #4a4a4a;
 }
 </style>
